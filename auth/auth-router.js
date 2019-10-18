7f3cf855855650c89bf961e0ddab6cfe43054612
Tyler Biswell <tsbiswell@gmail.com>
2019-10-18 10:14:58 -0600

    implement the register and login functionality

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -9,10 +9,50 @@ router.post('/register', (req, res) => {
   let user = req.body;
   const hash = bcrypt.hashSync(user.password, 10);
   user.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      const token = generateToken(saved);
+
+      res.status(201).json({ user: saved, token });
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
 
 router.post('/login', (req, res) => {
   // implement login
+  const { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+
+        res.status(200).json({
+          message: `Welcome, ${user.username}`,
+          token,
+        });
+      } else {
+        res.status(401).json({ message: 'Improper login credentials' });
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+  };
+  const options = {
+    expiresIn: '1d',
+  };
+  return jwt.sign(payload, process.env.JWT_SECRET, options);
+}
 
 module.exports = router;
