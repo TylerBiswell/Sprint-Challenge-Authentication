e28f4051bf105e40204ca983b1e0a370d553c74a
Tyler Biswell <tsbiswell@gmail.com>
2019-10-18 10:08:46 -0600

    update models

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,14 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const Users = require('./auth-model');
 
 router.post('/register', (req, res) => {
   // implement registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
 });
 
 router.post('/login', (req, res) => {
